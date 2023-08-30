2b564ffcc7a260830420ce2ce1ffdf0fd4b84048
cihatata <omercihatataa@gmail.com>
2023-08-30 17:58:44 +0300

    change file name

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Cihat ATA",
   url: "https://cihatata.dev",
-  ogImage: "https://cihatata.dev/og.png",
+  ogImage: "https://cihatata.dev/og2.png",
   description:
     "Frontend Developer olarak çalışıyorum. Web uygulamaları geliştirmeyi seviyorum. Zaman buldukça yeni yerler keşfetmeye çalışıyorum. Deneyim paylaşımlarını değerli buluyorum. Burada, edindiğim deneyimleri ve çalışmalarımı paylaşmaya çalışacağım.",
   links: {
