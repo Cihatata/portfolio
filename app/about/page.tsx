3d52739f2eb8d52abcdbf02d596ddfa35f648bac
cihatata <omercihatataa@gmail.com>
2023-08-30 16:58:49 +0300

    replace og png

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,7 +17,7 @@ export default function About() {
           <TitleSecondary>Merhaba !</TitleSecondary>
           <Description className="md:max-w-3xl">
             Ben Cihat ATA. <a href="https://resmo.com" target="_blank" className="text-blue-500 font-semibold">Resmo</a>`da 
-            Frontend Developer olarak çalışıyorum. Ankara`da yaşıyorum. Gazi Üniversitesi Bilgisayar Mühendisliği mezunuyum.(20’) 
+            Frontend Developer olarak çalışıyorum. Ankara`da yaşıyorum. Gazi Üniversitesi Bilgisayar Mühendisliği mezunuyum(20’).
             3+ yıldır sektörde Frontend Developer olarak çalışıyorum.
             JavaScript teknolojileri ile ilgileniyorum. Ürün odaklı şirketlerde çalışmayı 
             tercih ediyorum. Öğrendiklerimi ve deneyimlerimi başkalarına aktarma konusunda 
