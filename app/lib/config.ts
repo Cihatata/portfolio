a7b53c3bfe080d37b1bb062ec1dad258a4061fb2
cihatata <omercihatataa@gmail.com>
2023-08-30 17:17:46 +0300

    change siteconfig

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -3,12 +3,11 @@ export const siteConfig = {
   url: "https://cihatata.dev",
   ogImage: "https://cihatata.dev/og.png",
   description:
-    "Fronted developer | Loves React and all things JavaScript",
+    "Frontend Developer olarak çalışıyorum. Web uygulamaları geliştirmeyi seviyorum. Zaman buldukça yeni yerler keşfetmeye çalışıyorum. Deneyim paylaşımlarını değerli buluyorum. Burada, edindiğim deneyimleri ve çalışmalarımı paylaşmaya çalışacağım.",
   links: {
     twitter: "https://twitter.com/cihatata",
     github: "https://github.com/cihatata",
     linkedin: "https://www.linkedin.com/in/cihat-ata",
-    cv: "/cv.pdf",
   },
 };
 
